57d7ba0fc5119269c488f5a08dea8af0ade3d835
paulbkim1 <paulbkim1@gmail.com>
2022-12-26 15:00:20 -0800

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var header = document.querySelector("header");
+const header = document.querySelector("header");
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const logo = document.querySelector(".logo");
 
 
 window.addEventListener("scroll", (event) => {
@@ -14,10 +15,12 @@ window.addEventListener("scroll", (event) => {
     hamburger.addEventListener("click", () => {
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
+        logo.classList.toggle("active");
         header.classList.toggle("stickyScroll", window.scrollY > -1);
 });
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    logo.classList.remove("active");
 }))
