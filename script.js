a117fb363119983079ebd9d498fa318e2b1155f0
paulbkim1 <paulbkim1@gmail.com>
2024-05-02 16:37:35 -0700

    update

--- script.js
+++ script.js
@@ -3,24 +3,29 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const logo = document.querySelector(".logo");
 
+window.onload = function () {
+  window.scrollTo(0, 0);
+};
 
 window.addEventListener("scroll", (event) => {
-    if (hamburger.classList.contains("active")) {
-        event.preventDefault();
-    } else {
-        header.classList.toggle("stickyScroll", window.scrollY > 0);
-    }
-    });
-
-    hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("active");
-        navMenu.classList.toggle("active");
-        logo.classList.toggle("active");
-        header.classList.toggle("stickyScroll", window.scrollY > -1);
+  if (hamburger.classList.contains("active")) {
+    event.preventDefault();
+  } else {
+    header.classList.toggle("stickyScroll", window.scrollY > 0);
+  }
 });
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  logo.classList.toggle("active");
+  header.classList.toggle("stickyScroll", window.scrollY > -1);
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
     logo.classList.remove("active");
-}))
+  })
+);
