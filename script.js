85d49cd9e9ee359351e5271af41f06b65e479a37
paulbkim1 <paulbkim1@gmail.com>
2024-05-05 17:36:03 -0700

    update

--- script.js
+++ script.js
@@ -29,3 +29,78 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     logo.classList.remove("active");
   })
 );
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slideElements = document.querySelectorAll(".slide-in");
+  const growElements = document.querySelectorAll(".grow-in");
+
+  function isVisible(element) {
+    const elementBox = element.getBoundingClientRect();
+    const distanceFromTop = -70;
+
+    return elementBox.top - window.innerHeight < distanceFromTop;
+  }
+
+  function scanDocument() {
+    slideElements.forEach((element) => {
+      if (isVisible(element)) {
+        element.classList.add("slide-visible");
+      } else {
+        element.classList.remove("slide-visible");
+      }
+    });
+
+    growElements.forEach((element) => {
+      if (isVisible(element)) {
+        element.classList.add("grow-visible");
+      } else {
+        element.classList.remove("grow-visible");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", scanDocument);
+});
+
+const texts = [
+  "Front End Developer",
+  "Problem Solver",
+  "Perpetual Learner",
+  "Dedicated Professional",
+];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById("typed").textContent = letter;
+  if (letter.length === currentText.length) {
+    setTimeout(erase, 2000);
+  } else {
+    const delay = letter.endsWith(" ") ? 30 : 110;
+    setTimeout(type, delay);
+  }
+}
+
+function erase() {
+  letter = currentText.slice(0, --index);
+  document.getElementById("typed").textContent = letter;
+
+  if (letter.length === 0) {
+    count++;
+    index = 0;
+    setTimeout(type, 120);
+  } else {
+    const delay = letter.endsWith(" ") ? 30 : 60;
+    setTimeout(erase, delay);
+  }
+}
+
+type();
